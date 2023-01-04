06eae03d2be49a7b3fba3de977ef0cbaa596a363
kr_cu <basketlinux@nate.com>
2023-01-04 19:23:11 +0900

    file read write

--- src/pythonFile.py
+++ src/pythonFile.py
@@ -37,3 +37,28 @@ with open('../resource/review.txt', 'r', encoding='utf-8') as f:
     for c in contents:
         print(c, end=' ***** ')
 
+with open('../resource/score.txt', 'r') as f:
+    score = []
+    for line in f:
+        score.append(int(line))
+    print(score)
+
+print('Average : {:6.3}'.format(sum(score)/len(score)))
+
+with open('../resource/text.txt', 'a') as f:
+    f.write('Goodman!!\n')
+
+from random import randint
+
+with open('../resource/text2.txt', 'w') as f:
+    for cnt in range(6):
+        f.write(str(randint(1,50)))
+        f.write('\n')
+
+with open('../resource/text3.txt', 'w') as f:
+    list = ['Kim\n', 'Park\n', 'Cho\n']
+    f.writelines(list)
+
+with open('../resource/text4.txt', 'w') as f:
+    print('Test Contests1!', file=f)
+    print('Test Contests2!', file=f)
